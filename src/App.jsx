0507cec147acf71709b38d26f898bfb7833746b0
hansode76 <maykelrigueti93@gmail.com>
2025-01-07 02:04:02 -0300

    general adjustments and favicon added

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Weather5days from "./components/Weather5days";
 function App() {
   const [weather, setWeather] = useState();
   const [weather5days, setWeather5days] = useState();
-  
+
   const inputRef = useRef();
 
   async function SearchCity() {
@@ -15,25 +15,25 @@ function App() {
     const key = "1171cc8e2920a8c261a4fbb3d127bdfc";
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`;
     const url5days = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${key}&lang=pt_br&units=metric`;
-    
-    const apiInfo = await axios.get(url)
-    const apiInfo5days = await axios.get(url5days)
 
-    setWeather5days(apiInfo5days.data)
-    setWeather(apiInfo.data)
+    const apiInfo = await axios.get(url);
+    const apiInfo5days = await axios.get(url5days);
+
+    setWeather5days(apiInfo5days.data);
+    setWeather(apiInfo.data);
   }
 
   return (
-  <div className="container">
-    <h1>Tempo360</h1>
+    <div className="container">
+      <h1>Tempo360</h1>
 
-    <input ref={inputRef} type="text" placeholder="Digite o nome do local" /> 
-    <button onClick={SearchCity}>Buscar</button>
+      <input ref={inputRef} type="text" placeholder="Digite o nome do local" />
+      <button onClick={SearchCity}>Buscar</button>
 
-    { weather && <InfoWeather weather={weather} /> }
-    { weather5days && <Weather5days weather5days={weather5days} /> }
-  </div>
-  )
+      {weather && <InfoWeather weather={weather} />}
+      {weather5days && <Weather5days weather5days={weather5days} />}
+    </div>
+  );
 }
 
 export default App;
